d61272dac1233e73b0be3361599672f9ffb57b29
Boden <boden.bradley@gmail.com>
2022-12-20 11:19:36 -0800

    stuff

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -1,24 +1,24 @@
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
+withAuthenticator,
+Button,
+Heading,
+Image,
+View,
+Card,
 } from "@aws-amplify/ui-react";
 
 function App({ signOut }) {
-  return (
+return (
     <View className="App">
-      <Card>
+    <Card>
         <Image src={logo} className="App-logo" alt="logo" />
         <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
+    </Card>
+    <Button onClick={signOut}>Sign Out</Button>
     </View>
-  );
+);
 }
 
 export default withAuthenticator(App);
